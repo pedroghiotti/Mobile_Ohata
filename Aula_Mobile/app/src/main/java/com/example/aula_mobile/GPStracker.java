f108e27f43bc4e84af7e11c2aae33945dada7173
pedroghiotti <67380577+pedroghiotti@users.noreply.github.com>
2022-04-04 15:28:55 -0300

    Classe GPStracker comentada.

--- Aula_Mobile/app/src/main/java/com/example/aula_mobile/GPStracker.java
+++ Aula_Mobile/app/src/main/java/com/example/aula_mobile/GPStracker.java
@@ -16,19 +16,30 @@ public class GPStracker implements LocationListener
 {
     Context context;
 
-    // ?
+    /*
+        Constructor da classe.
+        Pede o contexto da aplicação.
+        (A classe context dá acesso às funções do sistema do dispositivo, se eu entendi corretamente)
+    */
     public GPStracker(Context c)
     {
         context = c;
     }
 
-    //
+    /*
+        Utiliza as funções de localização do dispositivo para retornar sua localização
+        desde que a aplicação tenha a permissão do usuário e o GPS do dispositivo esteja ativo.
+    */
     public Location getLocation()
     {
-        // ?
+        /*
+            Verifica se o usuário deu a permissão para que o aplicativo acesse as funções de localização do dispositivo.
+        */
         if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
-            // ?
+            /*
+                Coloca na tela uma notificação avisando o usuário que o aplicativo não tem a permissão pra utilizar a função.
+            */
             Toast.makeText(context, "Não tem permissão", Toast.LENGTH_SHORT).show();
             return null;
         }
@@ -36,7 +47,11 @@ public class GPStracker implements LocationListener
         LocationManager locManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         boolean isGPSEnabled =  locManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
 
-        // ?
+        /*
+            Verifica se o GPS do dispositivo está habilitado.
+            Se sim, irá retornar a localização do dispositivo e
+            se não, coloca na tela uma notificação requisitando que o usuário o habilite.
+        */
         if(isGPSEnabled)
         {
             locManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 6000, 10, this);
